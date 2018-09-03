6d4595af474c0e1a2dc4c4c027c78537236856f4
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-03 09:51:56 +0200

    Add project files.

---
+++ CultureDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CultureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
